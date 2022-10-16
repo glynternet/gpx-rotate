ccde4798cd4f0523adbb3a6737ed6afea3db5897
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2022-10-16 13:42:42 +0100

    Remove all extensions during split

--- pkg/gpx/split.go
+++ pkg/gpx/split.go
@@ -13,6 +13,16 @@ func Split(gpx gpxgo.GPX) []gpxgo.GPX {
 	for _, track := range gpx.Tracks {
 		c := gpx
 		c.Name = track.Name
+		// remove all extensions, they sometimes cause import issues and have never been useful
+		track.Extensions = gpxgo.Extension{}
+		for i := range track.Segments {
+			// remove all extensions, they sometimes cause import issues and have never been useful
+			track.Segments[i].Extensions = gpxgo.Extension{}
+			for j := range track.Segments[i].Points {
+				// remove all extensions, they sometimes cause import issues and have never been useful
+				track.Segments[i].Points[j].Extensions = gpxgo.Extension{}
+			}
+		}
 		c.Tracks = []gpxgo.GPXTrack{track}
 		split = append(split, c)
 	}
