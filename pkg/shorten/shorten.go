e72329f61d8446d505bab45d8a9e5b804ad25117
Glyn Owen Hanmer <1295698+glynternet@users.noreply.github.com>
2024-11-08 14:02:50 +0000

    wip: gh/name-shorten

---
+++ pkg/shorten/shorten.go
@@ -0,0 +1,17 @@
+package shorten
+
+// Cap the length of some names
+func CapLength(max int) func(string) string {
+	panic("not implemented")
+}
+
+// Removes vowels from name
+// Need to have something that checks for empty names?
+func Devowel() string {
+	panic("not implemented")
+}
+
+// Indexer takes names in and spits out what they should be with an index at the end if the name has already been seen etc.
+func Indexer() func(string) string {
+	panic("not implemented")
+}
